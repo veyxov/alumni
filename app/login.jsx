ca32722f2d66974af130684683681ba7137066be
Shekhov Ismoil <veyxov@gmail.com>
2023-06-27 20:01:47 +0500

    feat: added login form fundamentals

---
+++ app/login.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import Link from "next/link"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+export function ProfileForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+}
+
--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Image from 'next/image'
-
+import { Button } from "@/components/ui/button"
+import { Login } from "../app/login.jsx"
 export default function Home() {
   return (
       <>
-        Hello world
+        Hello
+        <Login />
       </>
   )
 }
