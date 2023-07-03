11c29e98e0575f42b7bbfa731ebd379169341c1e
Shekhov Ismoil <veyxov@gmail.com>
2023-07-03 16:44:02 +0500

    feat: export login page to components

---
+++ ui/app/login/page.js
@@ -0,0 +1,7 @@
+import {Login} from '@/components/auth/login'
+
+export default function Page() {
+    return (<div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <Login />
+    </div>)
+}
--- ui/app/page.js
+++ ui/app/page.js
@@ -1,12 +1,7 @@
-
-import { Button } from "@/components/ui/button"
-import { Login } from "../app/login.jsx"
 export default function Home() {
   return (
       <>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <Login />
-      </div>
+      Hello this is the main page
       </>
   )
 }
