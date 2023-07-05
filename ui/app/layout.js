63cdfe526b7be41afa0e7abf1016e1d703ff0585
Shekhov Ismoil <veyxov@gmail.com>
2023-07-05 10:42:04 +0500

    feat: new header

--- ui/app/layout.js
+++ ui/app/layout.js
@@ -1,17 +1,43 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import  Link  from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Alumni',
-  description: 'Alumni review application',
+    title: 'Alumni',
+    description: 'Alumni review application',
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <div className="flex flex-col min-h-screen">
+            <header className="bg-white-800">
+                <div className="container mx-auto py-4 px-8 flex items-center justify-between">
+                    <Link href="/" className="text-black text-lg font-semibold">
+                        Alumni Reviews
+                    </Link>
+                    {/* Add your logo here if needed */}
+                    <div>
+                        <span className="mr-2">|</span>
+                        <Link href="/login" className="text-black font-bold m-4">
+                            Login
+                        </Link>
+                        <Link href="/register" className="text-black font-bold border-2 p-2 border-black rounded">
+                            Register
+                        </Link>
+                    </div>
+                </div>
+            </header>
+
+            <main className="container mx-auto px-4 py-8 flex-grow">{children}</main>
+
+            <footer className="bg-gray-800 text-white py-4">
+                <div className="container mx-auto px-8">
+                    <p className="text-center">© {new Date().getFullYear()} Alumni Reviews. All rights reserved.</p>
+                </div>
+            </footer>
+        </div></html>
+    )
 }
--- ui/app/page.js
+++ ui/app/page.js
@@ -1,63 +1,32 @@
-import Image from 'next/image'
-
 export default function Home() {
     const reviewsData = [
-        { id: 1, name: 'John Doe', review: 'Great platform to connect with alumni!', rating: 5 },
-        { id: 2, name: 'Jane Smith', review: 'I found my dream job through this platform. Highly recommended!', rating: 4 },
+        { id: 1, name: 'John Doe', position: 'Chief programmer in Netflix', image: "https://api.minimalavatars.com/avatar/random/png", rating: 5 },
+        { id: 2, name: 'Jane Smith', position: 'Elictrician-Engeneer', image: "https://api.minimalavatars.com/avatar/random/png", rating: 4 },
         // Add more review objects here
     ];
 
     return (
-        <div className="">
-            <div className="flex flex-col justify-between bg-gray-100 h-screen">
-                <header className="bg-white py-4 shadow">
-                    <div className="container mx-auto px-4">
-                        <nav className="flex items-center justify-between">
-                            <div>
-                                <h1 className="text-2xl font-bold">Alumni</h1>
-                            </div>
-                            <div>
-                                {/* Navigation links or user authentication */}
-                            </div>
-                        </nav>
-                    </div>
-                </header>
-
-                <main className="container mx-auto px-4 py-8">
-                    <section >
-                        <h2 className="text-2xl font-bold mb-4">Latest Reviews</h2>
-                        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                            {reviewsData.map((review) => (
-                                <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-6">
-                                    <div class="shrink-0 w-14">
-                                        {/* TODO: Use Next.js <Image /> */}
-                                        <img
-                                            src="https://api.minimalavatars.com/avatar/random/png"
-                                            alt="Picture of reviewer"
-                                        />
-                                    </div>
-                                    <div>
-                                        <div class="text-xl font-medium text-black">ChitChat</div>
-                                        <p class="text-slate-500">You have a new message!</p>
-                                    </div>
-                                </div>
-
-
-                            ))}
+        <>
+            <h2 className="text-2xl font-bold mb-4">Latest Reviews</h2>
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {reviewsData.map((review) => (
+                    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-6">
+                        <div className="shrink-0 w-14">
+                            {/* TODO: Use Next.js <Image /> */}
+                            <img
+                                src={review.image}
+                                alt="Picture of reviewer"
+                            />
                         </div>
-                    </section>
-                </main>
-
-
-                <footer className="bg-gray-200 py-4">
-                    <div className="container mx-auto px-4">
-                        <p className="text-center text-gray-600">
-                            &copy; 2023 Alumni Reviews. All rights reserved.
-                        </p>
+                        <div>
+                            <div className="text-xl font-medium text-black">{review.name}</div>
+                            <p className="text-slate-500">{review.position}</p>
+                        </div>
                     </div>
-                </footer>
 
+
+                ))}
             </div>
-        </div>
+        </>
     );
 }
