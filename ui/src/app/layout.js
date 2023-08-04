2a53f54cc0f92564c667e1cf21e2d9954cb99505
Shekhov Ismoil <veyxov@gmail.com>
2023-08-04 21:50:55 +0500

    feat: added bar

--- ui/src/app/layout.js
+++ ui/src/app/layout.js
@@ -1,17 +1,19 @@
+import Bar from './bar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Alumni',
-  description: 'Review site for alumnis',
+    title: 'Alumni',
+    description: 'Review site for alumnis',
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <Bar />
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
