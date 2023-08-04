f5eb8b55c1e659ff2bbdd4487609e1ff36d8b915
Shekhov Ismoil <veyxov@gmail.com>
2023-08-04 22:16:42 +0500

    feat: added tailwind forms and login page

--- ui/src/app/bar.js
+++ ui/src/app/bar.js
@@ -93,7 +93,7 @@ export default function Bar() {
                         </ul>
                         {/*      <!-- Actions --> */}
                         <div className="ml-auto flex items-center justify-end px-6 lg:ml-0 lg:flex-1 lg:p-0">
-                            <button > Login </button>
+                            <Link href="/auth/login">Login</Link>
                         </div>
                     </nav>
                 </div>
