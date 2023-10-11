0c67cead6feedf61baf447ae5295199e84fe2943
Shekhov Ismoil <veyxov@gmail.com>
2023-10-11 21:41:27 +0500

    add navbar

--- alumni-ui/src/App.jsx
+++ alumni-ui/src/App.jsx
@@ -1,11 +1,35 @@
-import './App.css'
+import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
 
-function App() {
+export default function App() {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+
   return (
-    <>
-      <div>hello</div>
-    </>
-  )
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+          <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  );
 }
-
-export default App
