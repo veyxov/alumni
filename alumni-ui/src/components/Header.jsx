d5c1bf879b75b595d898e40b9a5397a7973c6e67
Shekhov Ismoil <veyxov@gmail.com>
2023-10-13 22:55:18 +0500

    fix: header buttons

--- alumni-ui/src/components/Header.jsx
+++ alumni-ui/src/components/Header.jsx
@@ -1,46 +1,35 @@
-import { useDisclosure } from "@mantine/hooks";
-import { AppShell, Burger, Group, Skeleton } from "@mantine/core";
-import { Outlet } from "react-router-dom";
+import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Burger, Group, Button } from '@mantine/core';
+import { Outlet } from 'react-router-dom';
 
 export function Header() {
-  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
-  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+  const [opened, { toggle }] = useDisclosure();
 
   return (
     <AppShell
       header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: "sm",
-        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
-      }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { desktop: true, mobile: !opened } }}
       padding="md"
     >
       <AppShell.Header>
         <Group h="100%" px="md">
-          <Burger
-            opened={mobileOpened}
-            onClick={toggleMobile}
-            hiddenFrom="sm"
-            size="sm"
-          />
-          <Burger
-            opened={desktopOpened}
-            onClick={toggleDesktop}
-            visibleFrom="sm"
-            size="sm"
-          />
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Group justify="space-between" style={{ flex: 1 }}>
+      Utks
+            <Group ml="xl" gap={0} visibleFrom="sm">
+              <Button mr="xl" className={"text-white"}>Login</Button>
+              <Button className={"text-white"}>Register</Button>
+            </Group>
+          </Group>
         </Group>
       </AppShell.Header>
-      <AppShell.Navbar p="md">
-        Navbar
-        {Array(15)
-          .fill(0)
-          .map((_, index) => (
-            <Skeleton key={index} h={28} mt="sm" animate={false} />
-          ))}
+
+      <AppShell.Navbar py="md" px={4}>
+        <Button className={"text-white"}>Home</Button>
+        <Button className={"text-white"}>Blog</Button>
       </AppShell.Navbar>
-      <AppShell.Main><Outlet /></AppShell.Main>
+
+      <AppShell.Main><Outlet/ ></AppShell.Main>
     </AppShell>
   );
 }
