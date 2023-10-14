89fecdc766ee746bc0a1df5c9aaf62e70f112eab
Shekhov Ismoil <veyxov@gmail.com>
2023-10-14 16:04:59 +0500

    global: succesfully install tailwind

--- alumni-ui/src/main.jsx
+++ alumni-ui/src/main.jsx
@@ -2,16 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import "@mantine/core/styles.css";
-
-import { MantineProvider } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <MantineProvider>
     <BrowserRouter>
-      <App />
+        <App />
     </BrowserRouter>
-  </MantineProvider>,
 );
--- alumni-ui/tailwind.config.js
+++ alumni-ui/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-      content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,jsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
 
